8c6dd0ec04512f67e29f2aee3aad66f71db895ca
udit vaja <vajaudit91@gmail.com>
2024-11-07 00:36:06 +0530

    get hospital by id update

--- routes/v1/hospital/hospital.route.js
+++ routes/v1/hospital/hospital.route.js
@@ -9,5 +9,5 @@ const {  hospitalController } = require("../../../controllers");
 /* -------------------------- CREATE/SIGNUP DOCTOR ----------- */
 router.post("/create-hospital", hospitalController.createHospital);
 router.get("/get-hospitals", hospitalController.getHospitals);
-
+router.get("/get-hospital-by-id", hospitalController.getHospitalById);
 module.exports = router;
